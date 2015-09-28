c55dc77a7e5fc1509cd0c6876e4a33f7e62facf8
Martin Bruse <zond@google.com>
2015-09-28 17:37:47 +0200

    initial import

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float maxTurnRate = 1200f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/TrackingCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackingCamera : MonoBehaviour {
+
+	public Transform target;
+	public Vector3 distance = new Vector3(0f, 5f, -10f);
+
+	public float positionDamping = 2f;
+	public float rotationDamping = 2f;
+
+	private Transform thisTransform;
+
+	void Start () {
+		thisTransform = transform;
+	}
+	
+	void LateUpdate () {
+		if (!target) {
+			return;
+		}
+
+		Vector3 wantedPosition = target.position + (target.rotation * distance);
+		Vector3 currentPosition = Vector3.Lerp (thisTransform.position, wantedPosition, positionDamping * Time.deltaTime);
+		thisTransform.position = currentPosition;
+
+		Quaternion wantedRotation = Quaternion.LookRotation (target.position - thisTransform.position, target.up);
+		Quaternion currentRotation = Quaternion.Lerp(thisTransform.rotation, wantedRotation, rotationDamping * Time.deltaTime);
+		thisTransform.rotation = currentRotation;
+	}
+}
