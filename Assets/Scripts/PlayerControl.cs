7e212255a46903180f1f11b7a6c58271a4ef07b1
Martin Bruse <zond@google.com>
2015-09-29 07:19:29 +0200

    blabla

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -8,9 +8,41 @@ public class PlayerControl : MonoBehaviour {
 	public Vector3 velocity = Vector3.zero;
 	public float impulseSensitivity = 500f;
 	public float turnSensitivity = 1200f;
+	public float enginePowerValue = 0f;
 
 	public ParticleSystem leftExhaustFX;
 	public ParticleSystem rightExhaustFX;
+
+	private Vector3 impulse = Vector3.zero;
+	private float desiredImpulse = 0f;
+	private Vector3 impulseActual = Vector3.zero;
+	private float maxImpulseChange = 100f;
+	private Vector3 turnRate = Vector3.zero;
+	private float desiredImpulseInput = 0f;
+	private float desiredTurnXInput = 0f;
+	private float desiredTurnYInput = 0f;
+	private float desiredInputX = 0f;
+	private float desiredInputY = 0f; 
+
+	private Transform thisTransform;
+
+	public Vector3 Velocity() {
+		get
+		{
+				return velocity;
+		}
+	}
+
+	public Vector3 Impulse() {
+		get
+		{
+			return impulse;
+		}
+		set
+		{
+			impulse.x = Mathf.Clamp(value.x, 0, maxImpulse.x);
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
