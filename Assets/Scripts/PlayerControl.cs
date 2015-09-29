6f46666fc36d9823bd6159ae80528f652c9f84ed
Martin Bruse <zond@google.com>
2015-09-29 07:02:05 +0200

    exhausts

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,6 +4,13 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
 	public float maxTurnRate = 1200f;
+	public Vector3 maxImpulse = new Vector3(10f, 10f, 700f);
+	public Vector3 velocity = Vector3.zero;
+	public float impulseSensitivity = 500f;
+	public float turnSensitivity = 1200f;
+
+	public ParticleSystem leftExhaustFX;
+	public ParticleSystem rightExhaustFX;
 
 	// Use this for initialization
 	void Start () {
